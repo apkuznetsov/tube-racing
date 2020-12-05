420644f2603e720787d7469999dcedbed70a6ebb
Andrey <kouznts@ya.ru>
2020-12-05 11:54:16 +0400

    refactor background generator

--- Assets/Scripts/BackgroundGenerator.cs
+++ Assets/Scripts/BackgroundGenerator.cs
@@ -5,8 +5,8 @@ public class BackgroundGenerator : MonoBehaviour
     [SerializeField] private Transform container;
     [SerializeField] private GameObject[] prefab;
 
-    [SerializeField] private int numInstances;
-    [SerializeField] private float radius;
+    [SerializeField] private int numInstances = 10;
+    [SerializeField] private float radius = 1;
 
     // scaling
     // seed
@@ -23,11 +23,11 @@ public class BackgroundGenerator : MonoBehaviour
             var randomPrefabIndex = UnityEngine.Random.Range(0, prefab.Length);
             var quad = Instantiate(prefab[randomPrefabIndex]);
             
+            quad.transform.Rotate(Vector3.forward * Random.Range(-180, 180), Space.Self);
+            
             quad.transform.position = container.position + Random.onUnitSphere * radius;
             quad.transform.LookAt(container);
             quad.transform.forward = -quad.transform.forward;
-
-            quad.transform.Rotate(Vector3.forward * Random.Range(-180, 180), Space.Self);
         }
     }
 }
