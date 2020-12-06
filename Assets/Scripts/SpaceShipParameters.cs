4a1743c27b3ccd2e02dcf986bcc27b17d5ac0985
Andrey <kouznts@ya.ru>
2020-12-06 20:10:54 +0400

    impl space ship parameters

---
+++ Assets/Scripts/SpaceShipParameters.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu]
+public class SpaceShipParameters : ScriptableObject
+{
+    [SerializeField] public float thrustForce = 25000;
+    [SerializeField] public float torqueForce = 1000;
+
+    [SerializeField] public float maxLinearVelocity = 200;
+    [SerializeField] public float maxAngularVelocity = 1;
+}
