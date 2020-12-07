7dca8670b25f97f888a756b2e1f12613015f1082
Andrey <kouznts@ya.ru>
2020-12-07 09:41:28 +0400

    impl projectile script

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Timers;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float velocity;
+    [SerializeField] private float lifetime;
+
+    private float lifeTimer;
+    
+    private void Update()
     {
+        var thisTransform = transform;
+        Vector3 step = thisTransform.forward * (velocity * Time.deltaTime);
+
+        thisTransform.position += step;
+
+        lifeTimer += Time.deltaTime;
         
+        if (lifeTimer > lifetime)
+            OnLifetimeOver();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnLifetimeOver()
     {
-        
+        Destroy(gameObject);
     }
 }
