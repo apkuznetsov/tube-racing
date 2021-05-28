98467b1eda1ced82f75794fc5c41912ed2d2314a
apkuznetsov <kouznts@ya.ru>
2021-05-28 21:29:41 +0400

    car's fields

---
+++ TubeRace/Assets/Scripts/Car.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TubeRace
+{
+    public class Car : MonoBehaviour
+    {
+        [SerializeField] private float m_Mass;
+        [SerializeField] private string m_ModelName;
+        [SerializeField] private string m_EnginePower;
+        [SerializeField] private int m_NumSteeringWheels;
+        [SerializeField] private Color m_Color;
+    }
+}
