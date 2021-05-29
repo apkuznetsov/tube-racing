5073c1d09a76b8e6693b28ce06e11b992698ab1a
apkuznetsov <kouznts@ya.ru>
2021-05-29 11:03:19 +0400

    script car

--- TubeRace/Assets/Scripts/Car.cs
+++ TubeRace/Assets/Scripts/Car.cs
@@ -6,10 +6,24 @@ namespace TubeRace
 {
     public class Car : MonoBehaviour
     {
-        [SerializeField] private float m_Mass;
+        [SerializeField] 
+        [Range(0.0f, 100.0f)]
+        private float m_Mass;
+
+        [Multiline]
         [SerializeField] private string m_ModelName;
+
         [SerializeField] private string m_EnginePower;
+
+        [Range(0,10)]
         [SerializeField] private int m_NumSteeringWheels;
+
+        [HideInInspector]
         [SerializeField] private Color m_Color;
+
+        [SerializeField] private Vector3 m_Pos;
+        [SerializeField] private Quaternion m_Rotation;
+
+        [SerializeField] private Transform m_Wheel;
     }
 }
