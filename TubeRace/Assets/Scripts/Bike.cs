aec58e5d2c37b33f6c57289de6ebfb4933494d6c
apkuznetsov <kouznts@ya.ru>
2021-06-02 19:08:56 +0400

    script bike moving

--- TubeRace/Assets/Scripts/Bike.cs
+++ TubeRace/Assets/Scripts/Bike.cs
@@ -38,5 +38,42 @@ namespace TubeRace
         /// View
         /// </summary>
         [SerializeField] private BikeViewController visualController;
+
+        /// <summary>
+        /// Управление газом. Нормализованное. От -1 до +1
+        /// </summary>
+        private float forwardThrustAxis;
+
+        /// <summary>
+        /// Установка значения педали газа
+        /// </summary>
+        /// <param name="val"></param>
+        public void SetForwardThrustAxis(float val)
+        {
+            forwardThrustAxis = val;
+        }
+
+        /// <summary>
+        /// Управление отклонением влево и вправо. Нормализованное. От -1 до +1
+        /// </summary>
+        private float horizontalThrustAxis;
+        
+        public void SetHorizontalThrustAxis(float val)
+        {
+            horizontalThrustAxis = val;
+        }
+
+        private void MoveBike()
+        {
+            float currForwardVelocity =  forwardThrustAxis * bikeParameters.maxSpeed;
+            Vector3 forwardMoveDelta = transform.forward * (currForwardVelocity * Time.deltaTime);
+            
+            transform.position += forwardMoveDelta;
+        }
+        
+        private void Update()
+        {
+            MoveBike();
+        }
     }
 }
