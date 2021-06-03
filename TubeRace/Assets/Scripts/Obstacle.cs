56c53fe0be8b7ddece04ea1d8301ba5df607061e
apkuznetsov <kouznts@ya.ru>
2021-06-03 19:39:52 +0400

    script obstacle

--- TubeRace/Assets/Scripts/Obstacle.cs
+++ TubeRace/Assets/Scripts/Obstacle.cs
@@ -1,18 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstacle : MonoBehaviour
+namespace TubeRace
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Obstacle : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Track track;
+        [SerializeField] private float rollAngle;
+        [SerializeField] private float distance;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Range(0.0f, 20.0f)] [SerializeField] private float radiusModifier = 1.0f;
+
+        private void SetObstaclePoistion()
+        {
+            Vector3 obstaclePos = track.Position(distance);
+            Vector3 obstacleDir = track.Direction(distance);
+
+            Quaternion quater = Quaternion.AngleAxis(rollAngle, Vector3.forward);
+            Vector3 trackOffset = quater * (Vector3.up * (radiusModifier * track.Radius));
+
+            transform.position = obstaclePos - trackOffset;
+            transform.rotation = Quaternion.LookRotation(obstacleDir, trackOffset);
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.red;
+            Vector3 centerlinePos = track.Position(distance);
+            Gizmos.DrawSphere(centerlinePos, track.Radius);
+        }
+
+        private void OnValidate()
+        {
+            SetObstaclePoistion();
+        }
     }
 }
