6781e839d465aaeb60b0f92e7d4a2aeaef9371e7
apkuznetsov <kouznts@ya.ru>
2021-06-03 15:09:40 +0400

    script looped auto movement

--- TubeRace/Assets/Scripts/AutoLoopedMovement.cs
+++ TubeRace/Assets/Scripts/AutoLoopedMovement.cs
@@ -19,5 +19,39 @@ namespace TubeRace
         private float bikeDistance;
 
         private const float LoopDistance = 15.0f;
+
+        private void LoopBikePosition()
+        {
+            bool isEnd = (int) bikeDistance > (int) length - (int) LoopDistance;
+            bool isStart = (int) bikeDistance < (int) LoopDistance;
+
+            if (isEnd)
+                bikeDistance = LoopDistance;
+            else if (isStart)
+                bikeDistance = length - LoopDistance;
+
+            bike.position = start.position + direction.normalized * bikeDistance;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(start.position, end.position);
+        }
+
+        private void Start()
+        {
+            Vector3 vectorDifference = end.position - start.position;
+            length = vectorDifference.magnitude;
+            direction = vectorDifference.normalized;
+
+            bike.forward = direction;
+        }
+
+        private void Update()
+        {
+            bikeDistance += bikeSpeed;
+            LoopBikePosition();
+        }
     }
 }
