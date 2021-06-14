f6da1624db8f221247c1edad8dadb525d631df00
apkuznetsov <kouznts@ya.ru>
2021-06-14 18:22:02 +0400

    draw bezier curve gizmos

--- TubeRace/Assets/Scripts/Track/CurvedTrackPoint.cs
+++ TubeRace/Assets/Scripts/Track/CurvedTrackPoint.cs
@@ -1,10 +1,13 @@
-using System;
 using UnityEngine;
 
 namespace TubeRace
 {
     public class CurvedTrackPoint : MonoBehaviour
     {
+        [SerializeField] private float length = 1.0f;
+
+        public float Length => length;
+
         private void OnDrawGizmos()
         {
             Gizmos.color = Color.cyan;
--- TubeRace/Assets/Scripts/Track/TrackCurved.cs
+++ TubeRace/Assets/Scripts/Track/TrackCurved.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+
+#endif
 
 namespace TubeRace
 {
     public class TrackCurved : Track
     {
+        [SerializeField] private CurvedTrackPoint[] trackPoints;
+
         public override float Length()
         {
             return 1.0f;
@@ -17,6 +23,41 @@ namespace TubeRace
         public override Vector3 Direction(float distance)
         {
             return Vector3.zero;
+        }
+
+        private void DrawTrackPartGizmos(CurvedTrackPoint a, CurvedTrackPoint b)
+        {
+            Transform aTransform = a.transform;
+            Transform bTransform = b.transform;
+
+            Vector3 aPosition = aTransform.position;
+            Vector3 bPosition = bTransform.position;
+
+            float aLength = a.Length;
+            float bLength = b.Length;
+
+            Handles.DrawBezier(
+                aPosition,
+                bPosition,
+                aPosition + aTransform.forward * aLength,
+                bPosition - bTransform.forward * bLength,
+                Color.green, Texture2D.whiteTexture, 1.0f);
+        }
+
+        private void DrawBezierCurveGizmos()
+        {
+            if (trackPoints.Length < 3)
+                return;
+
+            for (int i = 0; i < trackPoints.Length - 1; i++)
+                DrawTrackPartGizmos(trackPoints[i], trackPoints[i + 1]);
+
+            DrawTrackPartGizmos(trackPoints[trackPoints.Length - 1], trackPoints[0]);
+        }
+
+        private void OnDrawGizmos()
+        {
+            DrawBezierCurveGizmos();
         }
     }
 }
