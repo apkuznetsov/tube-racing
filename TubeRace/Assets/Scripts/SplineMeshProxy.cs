b1a0d8d1b8c361c02c20054aa1e4e849644cd06e
apkuznetsov <kouznts@ya.ru>
2021-06-19 12:15:18 +0400

    update spline mesh's points

--- TubeRace/Assets/Scripts/SplineMeshProxy.cs
+++ TubeRace/Assets/Scripts/SplineMeshProxy.cs
@@ -1,13 +1,13 @@
 using SplineMesh;
 using UnityEngine;
-
 #if UNITY_EDITOR
 using UnityEditor;
+
 #endif
 
 namespace TubeRace
 {
-    #if UNITY_EDITOR
+#if UNITY_EDITOR
     [CustomEditor(typeof(SplineMeshProxy))]
     public class SplineMeshProxyEditor : Editor
     {
@@ -21,8 +21,8 @@ namespace TubeRace
             }
         }
     }
-    #endif
-    
+#endif
+
     [RequireComponent(typeof(Spline))]
     public class SplineMeshProxy : MonoBehaviour
     {
@@ -30,10 +30,28 @@ namespace TubeRace
 
         [SerializeField] private BezierTrackPoint pointA;
         [SerializeField] private BezierTrackPoint pointB;
+        private Spline spline;
+
+        private void Start()
+        {
+            spline = GetComponent<Spline>();
+        }
 
         public void UpdatePoints()
         {
+            SplineNode node0 = spline.nodes[0];
+            Transform transformA = pointA.transform;
+            Vector3 positionA = transformA.position;
 
+            node0.Position = positionA;
+            node0.Direction = positionA + transformA.forward * pointA.Length;
+
+            SplineNode node1 = spline.nodes[1];
+            Transform transformB = pointB.transform;
+            Vector3 positionB = transformB.position;
+
+            node1.Position = positionB;
+            node1.Direction = positionB + transformB.forward * pointB.Length;
         }
     }
 }
