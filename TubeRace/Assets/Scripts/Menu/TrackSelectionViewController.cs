a45f817bb91eec1bb6a47c93828680b66e60c20b
apkuznetsov <kouznts@ya.ru>
2021-06-28 16:55:08 +0400

    exit track selection view

--- TubeRace/Assets/Scripts/Menu/TrackSelectionViewController.cs
+++ TubeRace/Assets/Scripts/Menu/TrackSelectionViewController.cs
@@ -4,9 +4,17 @@ namespace TubeRace
 {
     public class TrackSelectionViewController : MonoBehaviour
     {
+        [SerializeField] private MainMenuViewController mainMenuViewController;
+        
         private void Awake()
         {
             gameObject.SetActive(false);
+        }
+        
+        public void OnButtonExit()
+        {
+            gameObject.SetActive(false);
+            mainMenuViewController.gameObject.SetActive(true);
         }
     }
 }
