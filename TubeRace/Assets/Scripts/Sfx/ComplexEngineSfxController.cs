1737712141223176350f368f0f7dd88895a6227b
apkuznetsov <kouznts@ya.ru>
2021-07-08 13:58:06 +0400

    refactor SFX

--- TubeRace/Assets/Scripts/Sfx/ComplexEngineSfxController.cs
+++ TubeRace/Assets/Scripts/Sfx/ComplexEngineSfxController.cs
@@ -1,39 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace TubeRace
 {
     public class ComplexEngineSfxController : MonoBehaviour
     {
+        private const float PitchFactor = 2f;
+
         [SerializeField] private Bike bike;
 
         [SerializeField] private AudioSource sfxLow;
         [SerializeField] private AudioSource sfxHigh;
         [SerializeField] private AudioSource sfxLoud;
-        
+        [SerializeField] private AudioSource sfxSonicBoom;
+
         [SerializeField] private AnimationCurve curveLow;
         [SerializeField] private AnimationCurve curveHigh;
         [SerializeField] private AnimationCurve curveLoud;
 
-        [SerializeField] private AudioSource sfxSonicBoom;
         [SerializeField] private float superSonicSpeed;
         [SerializeField] private AnimationCurve sonicCurve;
-        public bool IsSuperSonic { get; private set; }
+        private bool IsSuperSonic { get; set; }
 
-        public void SetSuperSonic(bool enable)
+        private void SetSuperSonic(bool enable)
         {
             if (!IsSuperSonic && enable)
-            {
                 sfxSonicBoom.Play();
-            }
-            
+
             IsSuperSonic = enable;
         }
-        
-        public const float PitchFactor = 2f;
-        
-        private void UpdateDynamicEngineSound()
+
+        private void UpdateSuperSonicSound()
+        {
+            SetSuperSonic(Mathf.Abs(bike.Velocity) > superSonicSpeed);
+
+            if (sfxSonicBoom.isPlaying)
+            {
+                float t = Mathf.Clamp01(sfxSonicBoom.time / sfxSonicBoom.clip.length);
+                sfxSonicBoom.volume = sonicCurve.Evaluate(t);
+            }
+        }
+
+        private void UpdateEngineSound()
         {
             if (IsSuperSonic)
             {
@@ -43,33 +50,22 @@ namespace TubeRace
 
                 return;
             }
-            
-            //float t = bike.NormalizedVelocity();
+
             float t = Mathf.Clamp01(bike.Velocity / superSonicSpeed);
 
             sfxLow.volume = curveLow.Evaluate(t);
             sfxLow.pitch = 1.0f + PitchFactor * t;
-            
+
             sfxHigh.volume = curveHigh.Evaluate(t);
             sfxHigh.pitch = 1.0f + PitchFactor * t;
 
             sfxLoud.volume = curveLoud.Evaluate(t);
         }
-        
+
         private void Update()
         {
-            SetSuperSonic(Mathf.Abs(bike.Velocity) > superSonicSpeed);
-
-            if (sfxSonicBoom.isPlaying)
-            {
-                float t = Mathf.Clamp01(sfxSonicBoom.time / sfxSonicBoom.clip.length);
-                sfxSonicBoom.volume = sonicCurve.Evaluate(t);
-            }
-            
-            UpdateDynamicEngineSound();
+            UpdateSuperSonicSound();
+            UpdateEngineSound();
         }
     }
 }
-
-
-
--- TubeRace/Assets/Scripts/Sfx/EngineSfxController.cs
+++ TubeRace/Assets/Scripts/Sfx/EngineSfxController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace TubeRace
@@ -9,19 +7,16 @@ namespace TubeRace
         [SerializeField] private Bike bike;
         [SerializeField] private AudioSource engineSource;
 
-        [Range(0.0f, 1.0f)]
-        [SerializeField] private float pitchVelocityModifier;
-        
-        private void UpdateEngineSoundSimple()
+        [Range(0.0f, 1.0f)] [SerializeField] private float pitchModifier;
+
+        private void UpdateEngineSound()
         {
-            engineSource.pitch = 1.0f + pitchVelocityModifier * bike.NormalizedVelocity();
+            engineSource.pitch = 1.0f + pitchModifier * bike.NormalizedVelocity();
         }
 
         private void Update()
         {
-            UpdateEngineSoundSimple();
+            UpdateEngineSound();
         }
     }
 }
-
-
